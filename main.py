1e80454b47c65d69d0d711e121050f01e482e575
Alex Arvanitidis <alex_arvanitidis@mail.ntua.gr>
2024-12-02 13:47:49 +0200

    fix: add jaqpot needed types

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from typing import Union
 
 from fastapi import FastAPI
+from jaqpot_api_client.models.prediction_request import PredictionRequest
+from jaqpot_api_client.models.prediction_response import PredictionResponse
+
+from src.tsemo import handle_tsemo
 
 app = FastAPI()
 
@@ -9,7 +13,7 @@ app = FastAPI()
 def read_root():
     return {"Hello": "World"}
 
+@app.post("/infer")
+def predict(req: PredictionRequest) -> PredictionResponse:
+    return handle_tsemo(req.dataset.input)
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
